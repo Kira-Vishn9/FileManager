f04a146b3677b7e51bb426206c31ac7dfbd041ca
Kira-Vishn9 <Kvishn9@gmail.com>
2023-06-26 14:03:00 -0400

    feat: fun call userName made get url

--- node.js
+++ node.js
@@ -1,1 +1,32 @@
-console.log('hellow world');
+const readline = require("readline");
+
+
+const args = process.argv.slice(2);
+const username = args.find(arg => arg.startsWith('--username=')).split('=')[1];
+
+console.log(`Welcome to the File Manager, ${username}!\n`);
+
+const getCurrentDirectory = () => process.cwd();
+
+function printCurrentDirectory() {
+    console.log(`You are currently in ${getCurrentDirectory()}\n`);
+}
+
+
+function getRl() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Enter a command: ',
+    });
+
+    return rl
+}
+
+function callPropmpt() {
+    rlGlobal.prompt()
+}
+
+const rlGlobal = getRl();
+printCurrentDirectory();
+callPropmpt()
